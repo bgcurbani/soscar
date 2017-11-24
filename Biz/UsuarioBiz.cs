d3b47109670f3ce54c5cbcb7ff7ecbb8281d831e
bgcurbani <31636925+bgcurbani@users.noreply.github.com>
2017-11-24 04:19:03 -0200

    Implementação regras negócio Usuário

--- Biz/UsuarioBiz.cs
+++ Biz/UsuarioBiz.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Objetos;
+using DataService;
 
 namespace Biz
 {
@@ -11,5 +13,64 @@ namespace Biz
     /// </summary>
     public class UsuarioBiz
     {
+        private UsuarioDataService usuarioDataService;
+
+        public UsuarioBiz()
+        {
+            usuarioDataService = new UsuarioDataService();
+        }
+
+        public bool VerificaSeEhFavorito(string sIDOficina, string sNomeUsuario, ref string sFavorito)
+        {
+            Usuario user = new Usuario();
+            user.Login = sNomeUsuario;
+
+            if (!usuarioDataService.UsuarioExiste(user))
+                return false;
+
+            bool bEhFavorito = false;
+            if (!usuarioDataService.LerFavorito(user.Codigo, sIDOficina, ref bEhFavorito))
+                return false;
+
+            if (bEhFavorito)
+                sFavorito = "s";
+            else
+                sFavorito = "n";
+
+            return true;
+        }
+
+        public bool FavoritarOficina(string sIDOficina, string sNomeUsuario, bool bFavoritar)
+        {
+            Usuario user = new Usuario();
+            user.Login = sNomeUsuario;
+
+            if (!usuarioDataService.UsuarioExiste(user))
+                return false;
+
+            if (bFavoritar)
+                return usuarioDataService.GravarFavorito(user.Codigo, sIDOficina);
+            else
+                return usuarioDataService.DeletarFavorito(user.Codigo, sIDOficina);
+        }
+
+        public bool LoginUsuario(Usuario user)
+        {
+            if (!usuarioDataService.LoginUsuario(user))
+                return false;
+
+            return true;
+        }
+
+        public bool CadastraUsuario(Usuario user)
+        {
+            if (usuarioDataService.UsuarioExiste(user))
+                return false;
+
+            if (usuarioDataService.CadastrarUsuario(user))
+                return true;
+                
+            return false;
+        }
     }
 }
