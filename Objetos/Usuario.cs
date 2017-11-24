24dc8150b8a7288a1a0c42aeec1c1c8c9b9e7582
bgcurbani <31636925+bgcurbani@users.noreply.github.com>
2017-11-24 04:30:25 -0200

    Adição de código para identificar usuário

--- Objetos/Usuario.cs
+++ Objetos/Usuario.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,8 @@ namespace Objetos
     public class Usuario
     {
         List<Oficina> favoritos;
+
+        public int Codigo { get; set; }
 
         public string Login { get; set; }
 
