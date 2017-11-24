f93ae7954f2e5a305a739acbb0acbb5ff8402116
bgcurbani <31636925+bgcurbani@users.noreply.github.com>
2017-11-24 04:15:43 -0200

    Implementação conexão banco de dados

---
+++ DataService/DBConnection.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService
+{
+    class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            _instance = new DBConnection();
+           return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+            if (Connection == null)
+            {
+                string connstring = string.Format("Server=localhost; database=soscar; UID=root; password=leader129");
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }        
+    }
+}
