9311ce74f48586c64c5cec243e83dded6ba90e09
bgcurbani <31636925+bgcurbani@users.noreply.github.com>
2017-11-24 04:18:05 -0200

    Implementação regras negócio Oficina

--- Biz/OficinaBiz.cs
+++ Biz/OficinaBiz.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataService;
+using Objetos;
 
 namespace Biz
 {
@@ -11,5 +13,71 @@ namespace Biz
     /// </summary>
     public class OficinaBiz
     {
+        private OficinaDataService dataService;
+
+        public OficinaBiz()
+        {
+            dataService = new OficinaDataService();
+        }
+
+        public bool CadastraAvaliacao(string sIDOficina, string sNomeUsuario, string sAvaliacao, int iNota)
+        {
+            UsuarioDataService userDataService = new UsuarioDataService();
+            Usuario user = new Usuario();
+            user.Login = sNomeUsuario;
+
+            if (!userDataService.UsuarioExiste(user))
+                return false;
+
+            Oficina of = new Oficina();
+            of.Id = sIDOficina;
+
+            if (!dataService.CadastrarAvaliacao(of, user, sAvaliacao, iNota))
+                return false;
+
+            return true;
+        }
+		
+		public bool DeletarAvaliacao(int iCodigoAvaliacao)
+		{
+			if (!dataService.DeletarAvaliacao(iCodigoAvaliacao))
+                return false;
+			
+			return true;
+		}
+		
+		public bool AtualizarAvaliacao(int iCodigoAvaliacao, string sAvaliacao, int iNota)
+		{
+			if (!dataService.AtualizarAvaliacao(iCodigoAvaliacao, sAvaliacao, iNota))
+                return false;
+			
+			return true;
+		}
+
+        public bool GetCodigoAvaliacoes(string sIDOficina, ref string sCodigos)
+        {
+            if (!dataService.GetcodigoAvaliacoes(sIDOficina, ref sCodigos))
+                return false;
+
+            return true;
+        }
+
+        public bool LeAvaliacao(string sCodigoAvaliacao, ref string sUsuario, ref string sAvaliacao, ref string sNota)
+        {
+            int iCodigoUsuario = 0;
+            if (!dataService.LeAvaliacao(sCodigoAvaliacao, ref iCodigoUsuario, ref sAvaliacao, ref sNota))
+                return false;
+
+            UsuarioDataService userDataService = new UsuarioDataService();
+            Usuario user = new Usuario();
+            user.Codigo = iCodigoUsuario;
+
+            if (!userDataService.LeUsuario(user))
+                return false;
+
+            sUsuario = user.Login;
+
+            return true;
+        }
     }
 }
